141de11a7a647b8d83b8b7d27d58bb0467df230f
Oscar Anillo <oscarani14@gmail.com>
2023-05-15 20:38:36 -0500

    add axios to call endpoint

--- src/Components/Notes-Components.jsx
+++ src/Components/Notes-Components.jsx
@@ -1,14 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from 'axios'
+
 export const NotesComponents = () => {
+    const [userInput, setUserInput] = useState("");
+    const [notesData, setNotesData] = useState([])
     const date =  new Date();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if(!userInput){
+            alert("Please add a note");
+            return;
+        }
+        axios.post("http://localhost:3005/api/notes", {
+            note: userInput
+        }).then((res) => {
+            setNotesData(notesData.concat(res.data))
+
+        })
+    }
+
+    useEffect(() => {
+        axios
+        .get("http://localhost:3005/api/notes")
+        .then((res) => {
+            console.log(res.data)
+            setNotesData(notesData.concat(res.data))
+        })
+        .catch( console.log )
+    }, [setNotesData])
 
     return (
         <div className="note-div">
             <h1>Today</h1>
             <p>{date.toDateString()}</p>
-            <form>
-                <input type="text" placeholder="" />
+            <form onSubmit={submitHandler}>
+                <input type="text" placeholder="Add a note..." value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
                 <button>Send</button>
             </form>
+            {notesData.map((item) => (
+                <div key={item._id}>
+                    <li>{item.note}</li>
+                    <p>{date.toLocaleTimeString()}</p>
+                </div>
+
+            ))}
         </div>
     )
 } 
